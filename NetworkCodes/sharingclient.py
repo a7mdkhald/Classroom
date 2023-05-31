05a7132fb07fddb47b0dc1bc2105af40bea6670b
a7md5ald55 <ahmedkhaled865675@gmail.com>
2023-05-31 22:33:53 +0300

    GUI2

--- NetworkCodes/sharingclient.py
+++ NetworkCodes/sharingclient.py
@@ -1,7 +1,7 @@
 from vidstream import ScreenShareClient
 import threading
 
-sender = ScreenShareClient('192.168.1.10',8080)
+sender = ScreenShareClient('192.168.1.10',9000)
 t= threading.Thread(target=sender.start_stream)
 
 t.start()
